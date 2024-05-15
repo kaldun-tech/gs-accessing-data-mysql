cbd45127611db538db703483df919457a51c6985
Taras <tsmereka@protonmail.com>
2024-05-14 20:56:25 -0500

    Comment updates

--- initial/src/main/java/com/example/accessingdatamysql/UserRepository.java
+++ initial/src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.accessingdatamysql.User;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
+/* Spring automatically implements this class into a Bean called userRepository
+ * CRUD: Create, Read, Update, Delete */
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
